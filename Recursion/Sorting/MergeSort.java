1e863892e11e0400b07d6f7ba7b2e78749dda08d
Muskan Kumar <muskankumar952593@gmail.com>
2025-10-16 19:41:55 +0530

    Merge Sort Using Recursion

--- Recursion/Sorting/MergeSort.java
+++ Recursion/Sorting/MergeSort.java
@@ -5,7 +5,11 @@ public class MergeSort{
         int[] arr = {5,4,3,2,1};
         arr = mergeSort(arr);
         System.out.println(Arrays.toString(arr));
+
+        mergeSortInPlace(arr, 0, arr.length);
+        System.out.println(Arrays.toString(arr));
     }
+
     static int[] mergeSort(int[] arr){
         if(arr.length == 1){
             return arr;
@@ -54,5 +58,59 @@ public class MergeSort{
 
         return mix;
     }
+
+
+    static void mergeSortInPlace(int[] arr, int start, int end){
+        if(end-start == 1){
+            return;
+        }
+
+        int mid = (start + end)/2;
+
+        mergeSortInPlace(arr, start, mid);
+        mergeSortInPlace(arr, mid, end);
+
+        mergeInPlace(arr, start, mid, end);
+    }
+
+    private static void mergeInPlace(int[] arr, int start, int mid, int end){
+        int[] mix = new int[end - start];
+
+        int i = start;
+        int j = mid;
+        int k = 0;
+
+        while(i < mid && j < end){
+            if(arr[i] < arr[j]){
+                mix[k] = arr[i];
+                i++;
+            }
+            else{
+                mix[k] = arr[j];
+                j++;
+            }
+            k++;
+        }
+
+        // it may be possible that one of the arrays is not complete
+        // copy the remaining elements
+        while(i < mid){
+            mix[k] = arr[i];
+            i++;
+            k++;
+        }
+
+        while(j < end){
+            mix[k] = arr[j];
+            j++;
+            k++;
+        }
+
+        for(int l=0; l<mix.length; l++){
+            arr[start+l] = mix[l];
+        }
+    }
+
+
 }
 
