c99fd8d0ee530cc07a2059b0fe414e859b8d1a93
Muskan Kumar <muskankumar952593@gmail.com>
2025-11-25 22:09:14 +0530

    SingleLinkedList Complete

---
+++ LinkedList/DoublyLinkedList.java
@@ -0,0 +1,35 @@
+
+
+public class DoublyLinkedList{
+
+}
+
+class LinkedList{
+
+    public void insertFirst(int val){
+        Node node = new Node(val);
+        node.next = head;
+        node.prev = null;
+        if(head != null){
+            head.prev = node;
+        }
+        head = node;
+    }
+
+
+    private class Node{
+        private int val;
+        private Node next;
+        private Node prev;
+
+        public Node(int val){
+            this.val = val;
+        }
+
+        public Node(int val, Node next, Node prev){
+            this.val = val;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+}
---
+++ LinkedList/SingleLinkedList.java
@@ -0,0 +1,170 @@
+
+public class SingleLinkedList{
+    public static void main(String[] args){
+        LinkedList list = new LinkedList();
+
+        list.insertLast(1);
+        list.insertLast(2);
+        list.insertLast(3);
+        list.insertLast(4);
+        list.insertLast(5);
+        list.insertFirst(0);
+        list.insert(100, 3);
+
+        list.display();
+        System.out.println("Size = " + list.size);
+
+        // System.out.println(list.deleteFirst());
+        // list.display();
+        // System.out.println("Size = " + list.size);
+
+        // System.out.println(list.deleteLast());
+        // list.display();
+        // System.out.println("Size = " + list.size);
+
+        System.out.println(list.delete(3));
+        list.display();
+        System.out.println("Size = " + list.size);
+    }
+};
+
+
+class LinkedList{
+    private Node head;
+    private Node tail;
+    public int size;
+
+    public LinkedList(){
+        this.size = 0;
+    }
+
+    public void insertFirst(int val){
+        Node node = new Node(val);
+        node.next = head;
+        head = node;
+
+        if(tail == null){
+            tail = head;
+        }
+
+        size += 1;
+    }
+
+    public void insertLast(int val){
+        if(tail == null){
+            insertFirst(val);
+            return;
+        }
+        Node node = new Node(val);
+        tail.next = node;
+        tail = node;
+        size++;
+    }
+
+    public void insert(int val, int index){
+        if(index == 0){
+            insertFirst(val);
+            return;
+        }
+        if(index == size){
+            insertLast(val);
+            return;
+        }
+        if(index > size){
+            System.out.println("Size out of bound");
+        }
+
+        Node temp = head;
+        for(int i = 1; i < index; i++){
+            temp = temp.next;
+        }
+
+        Node node = new Node(val, temp.next);
+        temp.next = node;
+        size++;
+    }
+
+    public int deleteFirst(){
+        int val = head.value;
+        head = head.next;
+        if(head == null){
+            tail = null;
+        }
+        size--;
+        return val;
+    }
+
+    public int deleteLast(){
+        if(size <= 1){
+            return deleteFirst();
+        }
+
+        Node secondLast = get(size - 2);
+        int val = tail.value;
+        tail = secondLast;
+        tail.next = null;
+        size--;
+        return val;
+    }
+
+    public int delete(int index){
+        if(index == 0){
+            return deleteFirst();
+        }
+        if(index == size - 1){
+            return deleteLast();
+        }
+
+        Node prev = get(index - 1);
+        int val = prev.next.value;
+
+        prev.next = prev.next.next;
+        size--;
+        return val;
+    }
+
+    // this is return node ( help to find node by value)
+    public Node find(int value){
+        Node node = head;
+        while( node != null){
+            if(node.value == value){
+                return node;
+            }
+            node = node.next;
+        }
+        return null;
+    }
+
+    // this is return idex of node (help to get index for delete)
+    public Node get(int index){
+        Node node = head;
+        for(int i = 0; i < index; i++){
+            node = node.next;
+        }
+        return node;
+    }
+
+    public void display(){
+        Node temp = head;
+        while(temp != null){
+            System.out.print(temp.value + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("END");
+
+    }
+
+    private class Node{
+        private int value;
+        private Node next;
+
+        public Node(int value){
+            this.value = value;
+        }
+
+        public Node(int value, Node next){
+            this.value = value;
+            this.next = next;
+        }
+    }
+}
